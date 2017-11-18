9303b4062bff09bcba814e9561536e77aeb17821
929856540.com@qq.com <929856540@qq.com>
2017-11-18 07:57:06 -0800

    BubbleSort not finish!

---
+++ Sort/BubbleSort.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+void BubbleSort(int* array,int size)
+{
+    assert(array);
+    for(int end = )
+    for(int i = 0;i<size;++i)
+    {
+        if(array[i] > array[i+1])
+            swap(array[i],array[i+1]);
+    }
+}
