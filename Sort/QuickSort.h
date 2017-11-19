e0cf8da0ac1f9822385bb83e0a53ae08b81bdc7d
929856540.com@qq.com <929856540@qq.com>
2017-11-19 07:32:06 -0800

    Complete Quick Sort

--- Sort/QuickSort.h
+++ Sort/QuickSort.h
@@ -5,8 +5,49 @@
 #include<assert.h>
 using namespace std;
 
-void QuickSort(int* array,int size)
+int PartSort(int* array,int left,int right)
 {
     assert(array);
+    int& key = array[right];
+    while(left < right)
+    {
+        while(left < right && array[left] <= key)
+            ++left;
+        while(left < right && array[right] >= key)
+            --right;
+
+        swap(array[left],array[right]);
+    }
+
+    swap(array[left],key);
+    return left;
+}
+
+void QuickSort(int* array,int left,int right)
+{
+    assert(array);
+    if(left >= right)
+        return;
+    
+    int index=PartSort(array,left,right);
+    QuickSort(array,left,index-1);
+    QuickSort(array,index+1,right);
+}
+
+void  Print(int* array,int size)
+{
+    assert(array);
+    for(int i = 0;i<size;++i)
+    {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void TestQuickSort()
+{
+    int array[] = {1,3,7,0,4,8,2,6,9,5};
+    QuickSort(array,0,sizeof(array)/sizeof(array[0])-1);
+    Print(array,sizeof(array)/sizeof(array[0]));
 }
 #endif
--- Sort/Test.cpp
+++ Sort/Test.cpp
@@ -1,12 +1,14 @@
 //#include"BubbleSort.h"
 //swapfile#include"SelectSort.h"
-#include"HeapSort.h"
+//#include"HeapSort.h"
+#include"QuickSort.h"
 using namespace std;
 
 int main()
 {
  //   TestBubbleSort();
  //   TestSelectSort();
-    TestHeapSort();
+ //   TestHeapSort();
+    TestQuickSort();
     return 0;
 }
