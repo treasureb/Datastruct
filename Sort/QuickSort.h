30020b66b5b3a3b459ce087a4b840bb05e0a7b9c
929856540.com@qq.com <929856540@qq.com>
2017-11-26 03:57:15 -0800

    QuickSort Not Recursion

--- Sort/QuickSort.h
+++ Sort/QuickSort.h
@@ -2,12 +2,42 @@
 #define __QUICKSORT__
 
 #include<iostream>
+#include"InsertSort.h"
+#include<stack>
 #include<assert.h>
 using namespace std;
 
-int PartSort(int* array,int left,int right)
+int GetMid(int* array,int left,int right)
 {
     assert(array);
+    int mid = left + ((right - left)>>1);
+    if(array[left] <= array[right])
+    {
+        if(array[mid] <  array[left])
+            return left;
+        else if(array[mid] > array[right])
+            return right;
+        else
+            return mid;
+    }
+    else
+    {
+        if(array[mid] < array[right])
+            return right;
+        else if(array[mid] > array[left])
+            return left;
+        else
+            return mid;
+    }
+
+}
+
+int PartSort1(int* array,int left,int right)
+{
+    assert(array);
+    int mid = GetMid(array,left,right);
+    swap(array[mid],array[right]);
+    
     int& key = array[right];
     while(left < right)
     {
@@ -23,15 +53,92 @@ int PartSort(int* array,int left,int right)
     return left;
 }
 
+int PartSort2(int* array,int left,int right)
+{
+    assert(array);
+    int mid = GetMid(array,left,right);
+    swap(array[mid],array[right]);
+    
+    int key = array[right];
+    while(left < right)
+    {
+        while(left < right && array[left] <= key)
+            ++left;
+        array[right] = array[left];
+       
+        while(left < right && array[right] >= key)
+            --right;
+        array[left] = array[right];
+    }
+    array[right] = key;
+    return right;
+}
+
+int PartSort3(int* array,int left,int right)
+{
+    assert(array);
+    int mid = GetMid(array,left,right);
+    swap(array[mid],array[right]);
+    if(left < right)
+    {
+        int key = array[right];
+        int cur = left;
+        int pre = left - 1;
+        while(cur < right)
+        {
+            while(array[cur] < key && ++pre != cur)
+            {
+                swap(array[cur],array[pre]);
+            }
+            ++cur;
+        }
+        swap(array[++pre],array[right]);
+        return pre;
+    }
+    return -1;
+}
+
 void QuickSort(int* array,int left,int right)
 {
     assert(array);
     if(left >= right)
         return;
+
+    if((right - left) <= 5)
+    InsertSort(array,right-left+1);
     
-    int index=PartSort(array,left,right);
+    int index = PartSort3(array,left,right);
     QuickSort(array,left,index-1);
     QuickSort(array,index+1,right);
+}
+
+
+void QuickSortNotR(int* array,int left,int right)
+{
+    assert(array);
+    stack<int> s;
+    s.push(left);
+    s.push(right);
+    while(!s.empty())
+    {
+        int right = s.top();
+        s.pop();
+        int left = s.top();
+        s.pop();
+
+        int index = PartSort3(array,left,right);
+        if((index - 1) > left)
+        {
+            s.push(left);
+            s.push(index -1);
+        }  
+        if((index + 1) < right)
+        {
+            s.push(index + 1);
+            s.push(right);
+        }
+    }
+
 }
 
 void  Print(int* array,int size)
@@ -46,8 +153,12 @@ void  Print(int* array,int size)
 
 void TestQuickSort()
 {
-    int array[] = {1,3,7,0,4,8,2,6,9,5};
+    int array[] = {23,765,234,123,7,54,9,45,98};
     QuickSort(array,0,sizeof(array)/sizeof(array[0])-1);
     Print(array,sizeof(array)/sizeof(array[0]));
+
+    int array2[] = {23,765,234,123,7,54,9,45,98};
+    QuickSortNotR(array2,0,sizeof(array2)/sizeof(array2[0] -1));
+    Print(array2,sizeof(array2)/sizeof(array2[0]));
 }
 #endif
