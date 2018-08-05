e1d2b294f975b805d6c0e73720252cbf164b73dc
treasureb <929856540@qq.com>
2018-08-05 21:00:41 +0800

    scoped_ptr,防拷贝的做法

--- SmartPtr/ScopedPtr.h
+++ SmartPtr/ScopedPtr.h
@@ -10,6 +10,10 @@ public:
 	:_ptr(ptr)
 	{}
 
+    //c++11 中可以采用delete关键字来拒绝拷贝
+    ScopedPtr(ScopedPtr<T>& sp) = delete;
+    ScopedPtr<T> operator=(ScopedPtr<T> sp) = delete;
+
 	~ScopedPtr()
 	{
 		printf("ScopedPtrdelete:0x%p", _ptr);
@@ -28,15 +32,16 @@ public:
 	
 private:
 	
-	ScopedPtr<T>& operator=(const ScopedPtr<T>&);
-	ScopedPtr(ScopedPtr<T>&);//防止链接用户自己链接失败后定义
+	//ScopedPtr<T>& operator=(const ScopedPtr<T>&);
+	//ScopedPtr(ScopedPtr<T>&);//防止链接用户自己链接失败后定义
 private:
 	T* _ptr;
 };
 
 void TestScopedPtr()
 {
-	//ScopedPtr<int> sp1(new int(10));
-	//ScopedPtr<int> sp2(sp1);
+    //编译时直接报错
+	ScopedPtr<int> sp1(new int(10));
+	ScopedPtr<int> sp2(sp1);
 
 }
